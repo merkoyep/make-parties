8aa537de3a1af0a7e1b866d01d94387cc5bd765f
merkoyep <81783308+merkoyep@users.noreply.github.com>
2024-03-25 18:04:42 -0700

    Users can see all mock events

--- app.js
+++ app.js
@@ -18,9 +18,32 @@ app.engine(
 );
 app.set('view engine', 'handlebars');
 
-// Home route
+// Routes
+// OUR MOCK ARRAY OF PROJECTS
+const events = [
+  {
+    title: 'I am your first event',
+    desc: 'A great event that is super fun to look at and good',
+    imgUrl:
+      'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
+  },
+  {
+    title: 'I am your second event',
+    desc: 'A great event that is super fun to look at and good',
+    imgUrl:
+      'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
+  },
+  {
+    title: 'I am your third event',
+    desc: 'A great event that is super fun to look at and good',
+    imgUrl:
+      'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
+  },
+];
+
+// INDEX
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool! ' });
+  res.render('events-index', { events: events });
 });
 
 // port
