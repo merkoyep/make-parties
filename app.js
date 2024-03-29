5c2b384367d862d4cff1c7a6d8759dccdfddf444
merkoyep <81783308+merkoyep@users.noreply.github.com>
2024-03-29 16:46:08 -0700

    proper separation of concerns

--- app.js
+++ app.js
@@ -33,88 +33,9 @@ app.set('view engine', 'handlebars');
 // Routes
 
 // INDEX
-app.get('/', async (req, res) => {
-  const events = await prisma.event.findMany();
-  res.render('events-index', { events: events });
-});
+require('./controllers/events')(app, models);
 
 // EVENTS
-app.get('/events', async (req, res) => {
-  const events = await prisma.event.findMany();
-  console.log(events);
-  res.render('events-index', { events: events });
-});
-
-app.get('/events/new', (req, res) => {
-  res.render('events-new', {});
-});
-
-app.get('/events/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const event = await prisma.event.findUnique({
-      where: { id: parseInt(id, 10) },
-    });
-
-    if (event) {
-      res.render('events-show', { event });
-    } else {
-      res.status(404).send('Event not found');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error');
-  }
-});
-
-app.get('/events/:id/edit', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const event = await prisma.event.findUnique({
-      where: { id: parseInt(id, 10) },
-    });
-
-    if (event) {
-      res.render('events-edit', { event });
-    } else {
-      res.status(404).send('Event not found');
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error');
-  }
-});
-
-app.post('/events', async (req, res) => {
-  const { title, description, imgUrl } = req.body;
-  const result = await prisma.event.create({
-    data: {
-      title,
-      description,
-      imgUrl,
-    },
-  });
-  res.redirect('/events');
-});
-
-app.put('/events/:id', async (req, res) => {
-  const { id } = req.params;
-  const { title, description, imgUrl } = req.body;
-  try {
-    const updatedEvent = await prisma.event.update({
-      where: { id: parseInt(id, 10) },
-      data: {
-        title,
-        description,
-        imgUrl,
-      },
-    });
-    res.redirect(`/events/${id}`);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error');
-  }
-});
 
 // port
 const port = process.env.PORT || 3000;
