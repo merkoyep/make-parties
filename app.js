4a8541d4c939b535b92786f2183076d238ed1103
merkoyep <81783308+merkoyep@users.noreply.github.com>
2024-03-29 16:38:35 -0700

    Users can edit and update events

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // Initialize
 const express = require('express');
+const methodOverride = require('method-override');
 const app = express();
 const { PrismaClient } = require('@prisma/client');
 
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Method override
+app.use(methodOverride('_method'));
 
 // Initialize prisma client
 const prisma = new PrismaClient();
@@ -63,6 +67,24 @@ app.get('/events/:id', async (req, res) => {
   }
 });
 
+app.get('/events/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const event = await prisma.event.findUnique({
+      where: { id: parseInt(id, 10) },
+    });
+
+    if (event) {
+      res.render('events-edit', { event });
+    } else {
+      res.status(404).send('Event not found');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
 app.post('/events', async (req, res) => {
   const { title, description, imgUrl } = req.body;
   const result = await prisma.event.create({
@@ -73,6 +95,25 @@ app.post('/events', async (req, res) => {
     },
   });
   res.redirect('/events');
+});
+
+app.put('/events/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, description, imgUrl } = req.body;
+  try {
+    const updatedEvent = await prisma.event.update({
+      where: { id: parseInt(id, 10) },
+      data: {
+        title,
+        description,
+        imgUrl,
+      },
+    });
+    res.redirect(`/events/${id}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
 
 // port
